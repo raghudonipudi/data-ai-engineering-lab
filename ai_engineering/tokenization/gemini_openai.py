815c430924f4e8ed944bb589c4faaabaa337b02b
raghudonipudi <raghu.donipudi@gmail.com>
2026-05-02 19:51:41 +0530

    AI Engineering changes

---
+++ ai_engineering/tokenization/gemini_openai.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(
+    api_key="AIzaSyCaLAp-hZDQqWLmI9eMlVN9i3oTlbqBQq4",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages = [
+        { "role": "user", "content": "Hi, who are you!"}
+    ]
+)
+
+print(response.choices[0].message.content)
