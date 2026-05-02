30a04c57af1a76e9934324dc6e58d4f2727c4fc4
raghudonipudi <raghu.donipudi@gmail.com>
2026-05-02 20:03:53 +0530

    AI Engineering changes (#1)
    AI Engineering changes (#1)

---
+++ ai_engineering/tokenization/try_with_openai.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages = [
+        { "role": "user", "content": "Hi, how are you today"}
+    ]
+)
+
+print(response.choices[0].message.content)
